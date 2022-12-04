3b4ebc0b7cbb3cda02512c25bc5268cd5a120d43
Caeden Statia <caeden.s@outlook.com>
2022-12-03 21:22:00 -0800

    Day 4

---
+++ Day4/Program.cs
@@ -0,0 +1,27 @@
+﻿// Part 1
+var input = await File.ReadAllTextAsync("input.txt");
+
+var expandedPairs = input.Split(Environment.NewLine)
+    .Select(pairs => pairs.Split(','))
+    .Select(elf => elf
+        .Select(range => range.Split('-'))
+        .Select(endpoints => endpoints
+            .Select(endpointsStr => int.Parse(endpointsStr))
+            .ToArray())
+        .Select(range => Enumerable.Range(range[0], range[1] - range[0] + 1))
+        .ToArray());
+
+var rendundantPairs = expandedPairs
+    .Where(section =>
+        section[0].All(x => section[1].Contains(x))
+        || section[1].All(x => section[0].Contains(x)));
+
+Console.WriteLine($"Pairs with redundant sections: {rendundantPairs.Count()}");
+
+// Part 2
+var overlappingPairs = expandedPairs
+    .Where( it =>
+    it[0].Any(x => it[1].Contains(x))
+    || it[1].Any(x => it[0].Contains(x)));
+
+Console.WriteLine($"Pairs with overlapping sections: {overlappingPairs.Count()}");
