92238e3bf309111194af879492b74ab61e64c4fd
Caeden117 <caeden.s@outlook.com>
2022-12-09 13:37:50 -0800

    lmao day 2 optimized

--- Day2/Program.cs
+++ Day2/Program.cs
@@ -1,40 +1,70 @@
-﻿// Part 1
-var input = await File.ReadAllTextAsync("input.txt");
+﻿using System.Diagnostics;
+var input = await File.ReadAllLinesAsync("input.txt");
 
-var matches = input
-    .Split(Environment.NewLine)
-    .Select(it => it.Split(' '))
-    .Select(it => (it[0], it[1]));
-
-var pointsWithRespondingShape = matches.Select(match => match switch
+// Part 1
+var pointsWithRespondingShape = 0;
+for (int i = 0; i < input.Length; i++)
 {
-    ("A", "X") => 1 + 3,
-    ("A", "Y") => 2 + 6,
-    ("A", "Z") => 3 + 0,
-    ("B", "X") => 1 + 0,
-    ("B", "Y") => 2 + 3,
-    ("B", "Z") => 3 + 6,
-    ("C", "X") => 1 + 6,
-    ("C", "Y") => 2 + 0,
-    ("C", "Z") => 3 + 3,
-    _ => throw new System.Diagnostics.UnreachableException("huh?")
-});
+    var line = input[i];
+    pointsWithRespondingShape += line[0] switch
+    {
+        'A' => line[2] switch
+        {
+            'X' => 1 + 3,
+            'Y' => 2 + 6,
+            'Z' => 3 + 0,
+            _ => throw new UnreachableException("huh?")
+        },
+        'B' => line[2] switch
+        {
+            'X' => 1 + 0,
+            'Y' => 2 + 3,
+            'Z' => 3 + 6,
+            _ => throw new UnreachableException("huh?")
+        },
+        'C' => line[2] switch
+        {
+            'X' => 1 + 6,
+            'Y' => 2 + 0,
+            'Z' => 3 + 3,
+            _ => throw new UnreachableException("huh?")
+        },
+        _ => throw new UnreachableException("huh?")
+    };
+}
 
-Console.WriteLine($"Sum of points from all games, given X/Y/Z are shapes to respond with: {pointsWithRespondingShape.Sum()}");
+Console.WriteLine($"Sum of points from all games, given X/Y/Z are shapes to respond with: {pointsWithRespondingShape}");
 
 // Part 2
-var pointsWithDeterminedResult = matches.Select(match => match switch
+var pointsWithDeterminedResult = 0;
+for (int i = 0; i < input.Length; i++)
 {
-    ("A", "X") => 3 + 0,
-    ("A", "Y") => 1 + 3,
-    ("A", "Z") => 2 + 6,
-    ("B", "X") => 1 + 0,
-    ("B", "Y") => 2 + 3,
-    ("B", "Z") => 3 + 6,
-    ("C", "X") => 2 + 0,
-    ("C", "Y") => 3 + 3,
-    ("C", "Z") => 1 + 6,
-    _ => throw new System.Diagnostics.UnreachableException("huh?")
-});
+    var line = input[i];
+    pointsWithDeterminedResult += line[0] switch
+    {
+        'A' => line[2] switch
+        {
+            'X' => 3 + 0,
+            'Y' => 1 + 3,
+            'Z' => 2 + 6,
+            _ => throw new UnreachableException("huh?")
+        },
+        'B' => line[2] switch
+        {
+            'X' => 1 + 0,
+            'Y' => 2 + 3,
+            'Z' => 3 + 6,
+            _ => throw new UnreachableException("huh?")
+        },
+        'C' => line[2] switch
+        {
+            'X' => 2 + 0,
+            'Y' => 3 + 3,
+            'Z' => 1 + 6,
+            _ => throw new UnreachableException("huh?")
+        },
+        _ => throw new UnreachableException("huh?")
+    };
+}
 
-Console.WriteLine($"Sum of points from all games, given X/Y/Z are the expected result of the games: {pointsWithDeterminedResult.Sum()}");
+Console.WriteLine($"Sum of points from all games, given X/Y/Z are the expected result of the games: {pointsWithDeterminedResult}");
